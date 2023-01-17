09eafd361790f9d1f0dee05a5f4648737aaa4a36
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-01-16 23:44:03 -0800

    Fix playerload

--- lua/includes/modules/playerload.lua
+++ lua/includes/modules/playerload.lua
@@ -1,13 +1,13 @@
-local load_queue = {}
+local loadQueue = {}
 
 hook.Add( "PlayerInitialSpawn", "GM_FullLoadQueue", function( ply )
-    load_queue[ply] = true
+    loadQueue[ply] = true
 end )
 
 hook.Add( "SetupMove", "GM_FullLoadInit", function( ply, _, cmd )
     if not loadQueue[ply] then return end
     if cmd:IsForced() then return end
 
-    load_queue[ply] = nil
+    loadQueue[ply] = nil
     hook.Run( "PlayerFullLoad", ply )
 end )
